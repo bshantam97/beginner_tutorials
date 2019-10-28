dfa9f022c940c3721a345c66c295b70f69f48b95
shantam bajpai <51986101+bshantam97@users.noreply.github.com>
2019-10-28 11:09:12 -0400

    Removed cpplint errors

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,22 +1,20 @@
 /**@copyright  MIT License (c) 2019 Shantam Bajpai
  * @file        main.cpp
  * @author      Shantam Bajpai
- * @brief       Implementation for main(Robot) class.
+ * @brief       Implementation for listener.cpp
  */
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
-	ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
 int main(int argc, char **argv) {
-
-	ros::init(argc, argv, "listener");
-	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-	ros::spin();
-	
-	return 0;
-} 
+  ros::init(argc, argv, "listener");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  ros::spin();
+  return 0;
+}
