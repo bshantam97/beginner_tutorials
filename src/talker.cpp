5bb099ff1b214ba03c0ec31ec59d330a1f6686c1
bshantam97 <bshantam97@gmail.com>
2019-11-08 16:38:06 -0500

    Worked on Task1: Updated talker to broadcast talk with parent world

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,13 +1,32 @@
-/**@copyright  MIT License (c) 2019 Shantam Bajpai
- * @file        main.cpp
- * @author      Shantam Bajpai
- * @brief       Implementation for the publisher
+/**
+ * MIT License
+
+ Copyright (c) [2019] [Shantam Bajpai]
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
  */
 
 /*Include header files.....................................................*/
 #include <beginner_tutorials/Change_String.h>
 #include <sstream>
 #include "changeString.hpp"
+#include <tf/transform_broadcaster.h>
 #include "ros/ros.h"
 /*
  * @brief: This is a boolean function that is used to change the string values
@@ -23,29 +42,29 @@ bool changeString(beginner_tutorials::Change_String::Request &request,
 }
 
 int main(int argc, char** argv) {
-  // The node initialization
+  /*The node initialization*/
   ros::init(argc, argv, "talker");
   /*Creating an object for the nodeHandle class which is
    used as the access point for communcation
    */
   ros::NodeHandle nh_;
-  // message frequency
+  /*message frequency*/
   int freq = 20;
-  // Initialize counter
+  /*Initialize counter*/
   int count = 0;
-  // Inputs the frequency
+  /*Inputs the frequency*/
   freq = atoi(argv[1]);
   ROS_DEBUG_STREAM("The frequency has been set" << freq);
-  // Check for negative input frequency
+  /*Check for negative input frequency*/
   if (freq < 0) {
     ROS_ERROR_STREAM("The frequency cannot be less than 0");
-    // Set an arbitrary value of frequency to make it positive
+    /*Set an arbitrary value of frequency to make it positive*/
     freq = 2;
     ROS_INFO_STREAM(
         "The frequency value is changed so as to print out "
                     "messages with a low frequency rate");
   }
-  // Check whether the frequency is above 0 or not
+  /*Check whether the frequency is above 0 or not*/
   if (freq == 0) {
     ROS_FATAL_STREAM(
         "The frequency cannot be zero as then no "
@@ -66,11 +85,40 @@ int main(int argc, char** argv) {
    * to change the output frequency of the messasged being printed
    */
   ros::Rate loop_rate(freq);
+  /*Declaring a broadcaster object, provides an easy way to publish
+   co-ordinate frame transform information
+   */
+  static tf::TransformBroadcaster tbr;
+
+  /*Transform object*/
+  tf::Transform transform;
   while (ros::ok()) {
+
+    /*Set the translational element*/
+    transform.setOrigin(tf::Vector3(1, 1, 1));
+
+    /*Declare Quaternion object*/
+    tf::Quaternion q;
+
+    /*Set quaternion using fixed axis RPY*/
+    q.setRPY(0, 0, 40);
+
+    /*Set the rotational element by quaternion*/
+    transform.setRotation(q);
+
+    /*
+     * @param: "world": name of the parent frame
+     * @param: "talk": name of the child frame
+     */
+    tbr.sendTransform(
+        tf::StampedTransform(transform, ros::Time::now(), "world", "talk"));
+
+    /*message string*/
     std_msgs::String msg;
     std::stringstream ss;
     ss << stringChange << count;
     msg.data = ss.str();
+    /*prints out the string*/
     ROS_INFO_STREAM(msg.data.c_str() << count);
     chatter_pub.publish(msg);
     ros::spinOnce();
